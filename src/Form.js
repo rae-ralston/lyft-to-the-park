08377d48466f18a28bf18d8bd01d00af0ea8fc9c
Rachel Ralston <rachelralston@gmail.com>
2018-01-31 13:52:31 -0800

    project set up plus form compoent skeleton

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="submit" />
+      </form>
+    )
+  }
+}
